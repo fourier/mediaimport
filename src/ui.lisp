6ecadad60803b5a3118b04e35fdc8eb92e618bbc
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2020-06-27 18:19:12 +0200

    Changed interaction mode to multiple to simplify UI

--- src/ui.lisp
+++ src/ui.lisp
@@ -191,7 +191,7 @@
                    :color-function 'color-file-candidate
                    :action-callback 'on-candidate-dblclick
                    :pane-menu candidates-menu
-                   :interaction :extended-selection
+                   :interaction :multiple-selection
                    :columns `((:title ,string.from-column 
                                :adjust :left 
                                :visible-min-width (:character 45))
