1c337236ec717e52b212f822e46467c26586ff48
Alexey Veretennikov <fourier@protonmail.ch>
2021-12-17 19:27:04 +0100

    BUILD: Updated windows build script
    Added support for LW8 and updated script with correct syntax.

--- delivery_win32.bat
+++ delivery_win32.bat
@@ -1,7 +1,12 @@
 @echo off
-if exists "C:\Program Files (x86)\LispWorks\lispworks-7-1-0-x86-win32.exe" (
+
+if exist "C:\Program Files\LispWorks\lispworks-8-0-0-x86-win32.exe" (
+   set LW_PATH="C:\Program Files\LispWorks\lispworks-8-0-0-x86-win32.exe" 
+) else if exist "C:\Program Files (x86)\LispWorks\lispworks-8-0-0-x86-win32.exe" (
+   set LW_PATH="C:\Program Files (x86)\LispWorks\lispworks-8-0-0-x86-win32.exe" 
+) else if exist "C:\Program Files (x86)\LispWorks\lispworks-7-1-0-x86-win32.exe" (
    set LW_PATH="C:\Program Files (x86)\LispWorks\lispworks-7-1-0-x86-win32.exe"
-) else if exists "C:\Program Files\LispWorks\lispworks-7-1-0-x86-win32.exe" (
+) else if exist "C:\Program Files\LispWorks\lispworks-7-1-0-x86-win32.exe" (
    set LW_PATH="C:\Program Files\LispWorks\lispworks-7-1-0-x86-win32.exe"
 ) else (
    echo "Unable to find LispWorks executable!"
